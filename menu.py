5568e75119f16bd85cb4dd5cb883269f378a2d2b
eyal29 <149869349+eyal29@users.noreply.github.com>
2023-12-17 17:27:30 +0100

    amelioration visuel

--- menu.py
+++ menu.py
@@ -10,26 +10,28 @@ automate=Automate()
 #menu central
 def menu():
     
-    print("\n"*2) 
-    print("----------------------------")
-    print("Select an option :")
-    print("1. Créer un automate")
-    print("2. Modifier un AEF")
-    print("3. Supprimer un  AEF")
-    print("4. Verification")
-    print("5. Ameliorer l'AEF")
-    print("6. Opération sur l'AEF")
-    print("7. Exit")
-    print("----------------------------")
+    print("\n"*2)
+    print("\033[38;5;120m┌============================================┐\033[0m")
+    print("\033[38;5;120m|\033[0m              Select an option :            \033[38;5;120m|\033[0m")
+    print("\033[38;5;120m|\033[0m 1. ~Creation                               \033[38;5;120m|\033[0m")
+    print("\033[38;5;120m|\033[0m 2. ~Modification                           \033[38;5;120m|\033[0m")
+    print("\033[38;5;120m|\033[0m 3. ~Deletion                               \033[38;5;120m|\033[0m")
+    print("\033[38;5;120m|\033[0m 4. ~Verification                           \033[38;5;120m|\033[0m")
+    print("\033[38;5;120m|\033[0m 5. ~Improvement                            \033[38;5;120m|\033[0m")
+    print("\033[38;5;120m|\033[0m 6. ~Operation                              \033[38;5;120m|\033[0m")
+    print("\033[38;5;120m|\033[0m 7. ~Exit                                   \033[38;5;120m|\033[0m")
+    print("\033[38;5;120m└============================================┘\033[0m")
 
-    option = input("Faire votre choix>> ")
+
+    option = input("Select a number >> ")
     match(int(option)):
         case 1:
-            
+            print("\nWelcome in Creation!\n")
             automate.create()
         case 2:
             automate.modifier()
         case 3:
+            print("\nWelcome in Deletion!")
             automate.supprimer()
         case 4:
             verif()
@@ -41,7 +43,7 @@ def menu():
             print("Goodbye !")
             exit(0)
         case _:
-            print("Choix invalide. Choisir une option valide (1-7).")
+            print("Invalid choice. Choose a valid option (1-7).")
     menu()
         
 
@@ -49,15 +51,15 @@ def menu():
 def verif():
     print("\n"*5)
         
-    print("#######################")
-    print("Sélectionne une option de vérification :")
+    print("----------------------------------------------------------")
+    print("        Sélectionne une option de vérification :")
     print("1. Si un mot est reconnu")
     print("2. Si un automate est complet")
     print("3. Si un automate est deterministe")
     print("4. Si tous les cycles sont unitaires")
     print("5. L'equivalence entre 2 automates")
     print("6. Retour")
-    print("########################")
+    print("---------------------------------------------------------")
 
     option = input("Faites votre choix>> ")
     match(int(option)):
@@ -92,14 +94,14 @@ def improve():
 
     print("\n"*5)
 
-    print("|||||||||||||||||||||||||||")
+    print("************************************************************")
     print("Sélectionne une option pour améliorer l'AEF :")
     print("1. Rendre un automate complet")
     print("2. Rendre un automate deterministe")
     print("3. Rendre un automate émondé")
     print("4. Rendre un automate minimal")
     print("5. Retour")
-    print("|||||||||||||||||||||||||||")
+    print("************************************************************")
 
     option = input("Faites votre choix>> ")
     match(int(option)):
@@ -136,14 +138,14 @@ def improve():
 def operation():
     print("\n"*5)
 
-    print("|||||||||||||||||||||||||||")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("Sélectionne une option pour exécuter une opération sur l'AEF :")
     print("1. Complément d'un AEF")
     print("2. Miroir d'un AEF")
     print("3. Produit de 2 AEF")
     print("4. Concaténation de 2 AEF")
     print("5. Retour")
-    print("|||||||||||||||||||||||||||")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     
     option = input("Faites votre choix>> ")
     
